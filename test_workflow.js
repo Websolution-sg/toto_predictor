7b7599e14f53db2d93f73313f021c479e3d1729e
Websolution-sg <tongmingkwong@gmail.com>
2025-08-16 14:55:35 +0800

    TEST: Deleted latest result from CSV to test workflow fetching

---
+++ test_workflow.js
@@ -0,0 +1,6 @@
+console.log('🎯 TESTING WORKFLOW AFTER MANUAL CSV DELETION');
+console.log('📅 Current time:', new Date().toLocaleString());
+console.log('');
+
+// Import and run the main workflow
+require('./updateTotoCSV.cjs');
